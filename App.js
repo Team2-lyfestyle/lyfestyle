861f98caf3b965d25303ecbacc7a8707170b2fd8
Leonardo Yoshida <yoshida.leoy@gmail.com>
2020-03-09 16:50:15 -0700

    Implement navigation stack switching

--- App.js
+++ App.js
@@ -1,67 +1,207 @@
 import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
+import { Platform, StatusBar, StyleSheet, View, TextInput, Button } from 'react-native';
 import { SplashScreen } from 'expo';
 import * as Font from 'expo-font';
 import { Ionicons } from '@expo/vector-icons';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { AsyncStorage } from 'react-native';
+
 
 import BottomTabNavigator from './navigation/BottomTabNavigator';
+//import SignInScreen from './screens/SignInScreen';
+import SignUpScreen from './screens/SignUpScreen';
 import useLinking from './navigation/useLinking';
 
 const Stack = createStackNavigator();
+const AuthContext = React.createContext();
 
-export default function App(props) {
-  const [isLoadingComplete, setLoadingComplete] = React.useState(false);
-  const [initialNavigationState, setInitialNavigationState] = React.useState();
-  const containerRef = React.useRef();
-  const { getInitialState } = useLinking(containerRef);
-
-  // Load any resources or data that we need prior to rendering the app
-  React.useEffect(() => {
-    async function loadResourcesAndDataAsync() {
-      try {
-        SplashScreen.preventAutoHide();
-
-        // Load our initial navigation state
-        setInitialNavigationState(await getInitialState());
-
-        // Load fonts
-        await Font.loadAsync({
-          ...Ionicons.font,
-          'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
-        });
-      } catch (e) {
-        // We might want to provide this error information to an error reporting service
-        console.warn(e);
-      } finally {
-        setLoadingComplete(true);
-        SplashScreen.hide();
+export default function App({ navigation }) {
+  const [state, dispatch] = React.useReducer(
+    (prevState, action) => {
+      switch (action.type) {
+        case 'RESTORE_TOKEN':
+          return {
+            ...prevState,
+            userToken: action.token,
+            isLoading: false,
+          };
+        case 'SIGN_IN':
+          return {
+            ...prevState,
+            isSignout: false,
+            userToken: action.token,
+          };
+        case 'SIGN_OUT':
+          return {
+            ...prevState,
+            isSignout: true,
+            userToken: null,
+          };
       }
+    },
+    {
+      isLoading: true,
+      isSignout: false,
+      userToken: null,
     }
+  );
 
-    loadResourcesAndDataAsync();
+  React.useEffect(() => {
+    // Fetch the token from storage then navigate to our appropriate place
+    const bootstrapAsync = async () => {
+      let userToken;
+
+      try {
+        userToken = await AsyncStorage.getItem('userToken');
+      } catch (e) {
+        // Restoring token failed
+      }
+
+      // After restoring token, we may need to validate it in production apps
+
+      // This will switch to the App screen or Auth screen and this loading
+      // screen will be unmounted and thrown away.
+      dispatch({ type: 'RESTORE_TOKEN', token: userToken });
+    };
+
+    bootstrapAsync();
   }, []);
 
-  if (!isLoadingComplete && !props.skipLoadingScreen) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-        <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+  const authContext = React.useMemo(
+    () => ({
+      signIn: async data => {
+        // In a production app, we need to send some data (usually username, password) to server and get a token
+        // We will also need to handle errors if sign in failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+      signOut: () => dispatch({ type: 'SIGN_OUT' }),
+      signUp: async data => {
+        // In a production app, we need to send user data to server and get a token
+        // We will also need to handle errors if sign up failed
+        // After getting token, we need to persist the token using `AsyncStorage`
+        // In the example, we'll use a dummy token
+
+        dispatch({ type: 'SIGN_IN', token: 'dummy-auth-token' });
+      },
+    }),
+    []
+  );
+
+  return (
+    <AuthContext.Provider value={authContext}>
+      <NavigationContainer>
+      <Stack.Navigator>
+        {state.userToken == null ? (
+          <Stack.Screen name="SignIn" component={SignInScreen} />
+        ) : (
+          <Stack.Screen name="Root" component={BottomTabNavigator} />
+        )}
+      </Stack.Navigator>
+      </NavigationContainer>
+
+    </AuthContext.Provider>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+function SignInScreen() {
+  const [username, setUsername] = React.useState('');
+  const [password, setPassword] = React.useState('');
+
+  const { signIn } = React.useContext(AuthContext);
+
+  return (
+    <View>
+      <TextInput
+        placeholder="Username"
+        value={username}
+        onChangeText={setUsername}
+      />
+      <TextInput
+        placeholder="Password"
+        value={password}
+        onChangeText={setPassword}
+        secureTextEntry
+      />
+      <Button title="Sign in" onPress={() => signIn({ username, password })} />
+    </View>
+  );
+}
+
+
+// export default function App(props) {
+  
+//   const [isLoadingComplete, setLoadingComplete] = React.useState(false);
+//   const [initialNavigationState, setInitialNavigationState] = React.useState();
+//   const [isLoggedIn, setLoggedIn] = React.useState(false);
+
+//   const containerRef = React.useRef();
+//   const { getInitialState } = useLinking(containerRef);
+
+//   // Load any resources or data that we need prior to rendering the app
+//   React.useEffect(() => {
+//     async function loadResourcesAndDataAsync() {
+//       try {
+//         SplashScreen.preventAutoHide();
+
+//         // Load our initial navigation state
+//         setInitialNavigationState(await getInitialState());
+
+//         // Get Token
+//         token = await AsyncStorage.getItem('token')
+
+//         // Load fonts
+//         await Font.loadAsync({
+//           ...Ionicons.font,
+//           'space-mono': require('./assets/fonts/SpaceMono-Regular.ttf'),
+//         });
+//       } catch (e) {
+//         // We might want to provide this error information to an error reporting service
+//         console.warn(e);
+//       } finally {
+//         setLoadingComplete(true);
+//         if(token){setLoggedIn(true)}
+//         SplashScreen.hide();
+//       }
+//     }
+
+//     loadResourcesAndDataAsync();
+//   }, []);
+
+//   if (!isLoadingComplete && !props.skipLoadingScreen) {
+//     return null;
+//   } else {
+//     return (
+//       <View style={styles.container}>
+//         {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+//         <NavigationContainer ref={containerRef} initialState={initialNavigationState}>
+//           <Stack.Navigator>
+//             {
+//                 isLoggedIn ? 
+//                 (
+//                   <Stack.Screen name="Root" component={BottomTabNavigator} />
+//                 ) : 
+//                 (
+//                 <>
+//                   <Stack.Screen name="SignIn" component={SignInScreen}/>
+//                   <Stack.Screen name="SignUp" component={SignUpScreen}/>
+//                 </>
+//                 )
+//             }
+            
+//           </Stack.Navigator>
+//         </NavigationContainer>
+//       </View>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//   },
+// });

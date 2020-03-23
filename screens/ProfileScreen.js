985a68bf2079cf23b57eb9a7b2ad43973bf49799
Israel Perez <perezisrael1798@gmail.com>
2020-03-22 18:14:31 -0700

    Settings icon working

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -1,38 +1,53 @@
 import React from "react";
-import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView} from "react-native";
-import { Ionicons, MaterialIcons} from '@expo/vector-icons'
+import { StyleSheet, Text, View, SafeAreaView, Image, ScrollView, TouchableHighlight} from "react-native";
+import { Ionicons, MaterialIcons, AntDesign, FontAwesome} from '@expo/vector-icons'
+// import { Header } from "react-native/Libraries/NewAppScreen";
 
-export default function App() {
+export default function Profile({navigation}) {
     return (
         <SafeAreaView style = {styles.container}>
             {/* Allow for scroll on profile page */}
             <ScrollView showsVerticalScrollIndicator = {false}>
+                <TouchableHighlight style = {styles.settingsIcon} onPress={() => navigation.navigate('Home')}>
+                    <Ionicons name = "md-more" size = {32} color = "grey"/>
+                </TouchableHighlight>
+                
+                {/*
+                <View style = {[{marginTop: 20}, {marginRight: 20}, {alignItems: 'flex-end'}]}>
+                    <Ionicons name = "md-more" size = {32} color = "grey"/>
+                    <AntDesign name = "setting" size = {32} color = "grey"/> 
+                </View>
+                */}
+                
                 {/* Profile Image  */}
-                <View style = {[styles.centerThing, {marginTop: 30}]}>
+                <View style = {[styles.center, {marginTop: 10}]}>
                     <View style = {styles.profileImage}>
                         <Image source={require("../assets/images/profile_pic.jpg")} style = {styles.profileImage}></Image>
                     </View>
                 </View>
                 {/* Name */}
-                <View style = {[styles.centerThing, {marginTop: 30}, {marginBottom: 15}]}>
+                <View style = {[styles.center, {marginTop: 30}, {marginBottom: 15}]}>
                     <Text style = {[{fontSize: 30}, { fontWeight: '200' }]}>John Doe</Text>
-                    <View style = {[{marginTop: 15}, {alignItems: 'center'}]}>
+                    {/* Followers Count  */}
+                    {/* <View style = {[{marginTop: 15}, {alignItems: 'center'}]}>
                         <Text style = {{fontSize: 15}}>150</Text>
                         <Text style = {{fontSize: 10}, {color: '#AEB5BC'}}>Followers</Text>
-                    </View>
+                    </View> */}
                 </View>
                 {/* Bio  */}
-                <View style = {styles.centerThing2}>
+                <View style = {styles.centerBio}>
                     <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla tincidunt enim quis laoreet dapibus. Nulla lacinia posuere diam aliquam tincidunt. Vivamus commodo ligula quis nisl placerat laoreet.</Text>
                 </View>
-                {/* Stats Container; NOT WORKING  */}
+                {/* Stats Container */}
                 <View style = {styles.statsContainer}>
                     <View style = {[{alignItems: 'center'}, {marginTop: 10}]}>
-                        <Text style = {{fontSize: 15}}>100</Text>
-                        <Text style = {{fontSize: 10}, {color: '#AEB5BC'}}>Posts</Text>
+                        <Text style = {[{fontSize: 15}, { fontWeight: '200' }]}>100</Text>
+                        <Text style = {[{fontSize: 10}, { fontWeight: '200' }, {color: 'grey'}]}>Posts</Text>
                     </View>
                 </View>
+                {/* Picture Container  */}
                 <View style = {styles.pictureContainer}>
+                    {/* Scroll pictures in the horizontal direction  */}
                     <ScrollView horizontal = {true} showsHorizontalScrollIndicator = {false}>
                         <View style = {styles.mediaImage}>
                             <Image source = {require("../assets/images/1.jpg")} style = {styles.image} resizeMode="cover"></Image>
@@ -67,11 +82,18 @@ const styles = StyleSheet.create({
         flex: 1,
     },
 
-    centerThing: {
+    center: {
         alignSelf: 'center',
     },
 
-    centerThing2: {
+    settingsIcon: {
+        marginTop: 10,
+        marginRight: 20,
+        alignItems: 'flex-end'
+    },
+
+    // Properties to center Bio for profile
+    centerBio: {
         alignSelf: 'center',
         marginBottom: 20,
         marginLeft: 20,
@@ -80,10 +102,9 @@ const styles = StyleSheet.create({
 
     // Properties for the profile picture 
     profileImage: {
-        width: 200,
-        height: 200,
-        borderRadius: 100,
-        // overflow: 'hidden'
+        width: 180,
+        height: 180,
+        borderRadius: 90,
     },
 
     // Stats = Number of posts container 
@@ -98,7 +119,6 @@ const styles = StyleSheet.create({
 
     // Sets margin for image container 
     pictureContainer: {
-        // margin: 20
         marginTop: 20,
         marginBottom: 20
     },

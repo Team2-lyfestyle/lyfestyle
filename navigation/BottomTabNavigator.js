ff9eb1cb5475436fab929f2a489b0e3e5f2383fb
Leonardo Yoshida <yoshida.leoy@gmail.com>
2020-03-23 14:11:33 -0700

    Add imageupload tab to navigation for testing. REMOVE later

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -10,6 +10,8 @@ import ExploreScreen from '../screens/ExploreScreen';
 import ChatStackNavigator from '../navigation/ChatStackNavigator';
 import TipScreen from '../screens/TipScreen';
 import QueryExampleScreen from '../screens/QueryExampleScreen';
+import ImageUpload from '../screens/ImageUpload';
+
 
 const BottomTab = createBottomTabNavigator();
 const INITIAL_ROUTE_NAME = 'Home';
@@ -90,6 +92,16 @@ export default function BottomTabNavigator({ navigation, route }) {
           title: 'Queries',
           tabBarIcon: ({ focused }) => (
             <TabBarIcon focused={focused} name='add-circle-outline' />
+          )
+        }}
+      />
+      <BottomTab.Screen
+        name='Photo'
+        component={ImageUpload}
+        options={{
+          title: 'Photo',
+          tabBarIcon: ({ focused }) => (
+            <TabBarIcon focused={focused} name='ios-fitness' />
           )
         }}
       />

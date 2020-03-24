7c92ad078ad0da63fdeea4031a0ff4b20dcaeeeb
brandonkns66 <51934408+brandonkns66@users.noreply.github.com>
2020-03-23 22:43:17 -0700

    Moved async storage wrapper to sep file

---
+++ util/AsyncStorage.js
@@ -0,0 +1,37 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+const asyncStorage = {
+  setItem: async (key, value) => {
+    try {
+      if (!(typeof value === "string" || value instanceof String)) {
+        value = JSON.stringify(value);
+      }
+      console.log(`Setting ${key}: ${value}`);
+      await AsyncStorage.setItem(key, value);
+      return true;
+    }
+    catch (err) {
+      console.log(`Error setting ${key}: ${value}`, err);
+      return false;
+    }
+  },
+
+  getItem: async (key) => {
+    try {
+      console.log(`Getting value from ${key}`);
+      let value = await AsyncStorage.getItem(key);
+      if (value !== null) {
+        return value;
+      }
+      else {
+        // No value set at key
+        console.log(`No value set at ${key}`);
+        return null;
+      }
+    }
+    catch (err) {
+      console.log(`Error getting ${key}`, err);
+      return null;
+    }
+  },
+}

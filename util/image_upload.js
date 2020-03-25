74e2f1bbda46fc836a668994436469739e7b4fb9
Leonardo Yoshida <yoshida.leoy@gmail.com>
2020-03-25 13:41:58 -0700

    Improve image upload functions

--- util/image_upload.js
+++ util/image_upload.js
@@ -18,11 +18,11 @@ let uriToBlob = (uri) => {
     });
 }
 
-let uploadToFirebase = async (blob, fileName) => {
+let uploadToProfile = async (blob, fileName) => {
     let currentUser = await firebase.auth().currentUser.uid
     return new Promise((resolve, reject) => {
         var storageRef = firebase.storage().ref();
-        storageRef.child('uploads/' + currentUser + '/' + fileName).put(blob, {
+        storageRef.child('uploads/' + currentUser + '/profile/' + fileName).put(blob, {
             contentType: 'image/jpeg'
         }).then((snapshot) => {
             blob.close();
@@ -33,19 +33,45 @@ let uploadToFirebase = async (blob, fileName) => {
     });
 }
 
-let uploadImage = (result) => {
-    let uri = result.uri
-    var name = uri.split('/')
-    name = name[name.length-1]
-
-    uriToBlob(uri)
-    .then((blob) => {
-        return uploadToFirebase(blob, name);
-    }).then((snapshot) => {
-        console.log("File uploaded");
-    }).catch((error) => {
-        throw error;
+let uploadToPost = async (blob, postId) => {
+    let currentUser = await firebase.auth().currentUser.uid
+    return new Promise((resolve, reject) => {
+        var storageRef = firebase.storage().ref();
+        storageRef.child('uploads/' + currentUser + '/posts/' + postId + '.jpeg').put(blob, {
+            contentType: 'image/jpeg'
+        }).then((snapshot) => {
+            blob.close();
+            resolve(snapshot);
+        }).catch((error) => {
+            reject(error);
+        });
     });
 }
 
-export default uploadImage;
+module.exports = {
+    uploadProfileImage: (uri) => {
+        var name = uri.split('/')
+        name = name[name.length - 1]
+
+        uriToBlob(uri)
+            .then((blob) => {
+                return uploadToProfile(blob, name);
+            }).then((snapshot) => {
+                console.log("File uploaded");
+            }).catch((error) => {
+                throw error;
+            });
+    },
+
+    uploadPostImage: (uri, postId) => {
+        uriToBlob(uri)
+            .then((blob) => {
+                return uploadToPost(blob, postId);
+            }).then((snapshot) => {
+                console.log("File uploaded");
+            }).catch((error) => {
+                throw error;
+            });
+    }
+
+}
